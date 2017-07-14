dbf8ef07f0371c22ea3fd3d80b6da36e131beb00
shrutisinha <shrutisinha952@gmail.com>
2017-07-14 18:23:55 +0530

    soft coded searchfile

--- src/day9/assignment1/solution/SearchFile.java
+++ src/day9/assignment1/solution/SearchFile.java
@@ -1,5 +1,4 @@
-/**
- * Create an java program to search through the home directory and look for files that match
+/* Create an java program to search through the home directory and look for files that match
  * a regular expression. The program should be able to take inputs repeatedly and should print
  * out full absolute path of the matching files found.
  */
@@ -8,10 +7,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
 /**
  * Created by zemoso on 11/7/17.
@@ -28,26 +24,27 @@ public class SearchFile {
         InputStreamReader isr=new InputStreamReader(System.in);
         BufferedReader br=new BufferedReader(isr);
         String s;
+        FindPath path = new FindPath();
         while(true){
-            FindPath path = new FindPath();
             boolean found;
             System.out.println("Enter java regular expression to be matched with file name");
             System.out.println("Enter \"exit\" to exit");
             s = br.readLine();
-
             if (s.equals("exit"))break;
-
-            char seperator = File.separatorChar;
-            String directory=seperator+"home";
+            char separator = File.separatorChar;
+            String directory=separator+System.getProperty("user.home");
             found=path.findFiles(s, directory);
             List<File> filesearches;
-            filesearches=path.getResult();
+            filesearches=path.getmResultArray();
             if(!found){
                 System.out.println("No result found!");
             }
-            for(File file:filesearches){
-                System.out.println("Found:  "+file);
+            else{
+                for(File file:filesearches){
+                    System.out.println("Found:  "+file);
+                }
             }
+            path.clear();
         }
     }
 }
